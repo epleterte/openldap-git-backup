4ade4e5979a5c427d077c0eca57db83a1d99eace
Christian Bryn <chr.bryn@gmail.com>
2012-07-03 22:36:54 +0200

    Add license information.

--- backup-slapd-git.sh
+++ backup-slapd-git.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -ue
-# openldap/slapd backup with git
+# openldap/slapd revision controlled backup with git
 # supports config file for e-z administration
-# Christian Bryn 2012 <chr.bryn@gmail.com>
+#
+# Copyright (c) 2012 Christian Bryn <chr.bryn@gmail.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 ## default config
 backup_path="/srv/backup/ldap"
